a1743921f88acc8561f865c7d713d046d1ab7de3
xiaoxunyao <huchengyea@163.com>
2021-07-10 09:51:19 +0800

    🧂 add - Complete the resource configuration class

--- src/main/java/com/zfile/code/config/ResourcesConfig.java
+++ src/main/java/com/zfile/code/config/ResourcesConfig.java
@@ -5,6 +5,14 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+/**
+ * [资源配置类](Resource allocation)
+ * @description: zh - 资源配置类
+ * @description: en - Resource allocation
+ * @version: V1.0
+ * @author XiaoXunYao
+ * @since 2021/7/10 9:36 上午
+*/
 @Configuration
 public class ResourcesConfig implements WebMvcConfigurer {
 
