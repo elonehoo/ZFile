0fa746192f9fc4b722e79cd5cdcd50c985bc7f4d
xiaoxunyao <huchengyea@163.com>
2021-07-10 10:00:01 +0800

    🧂 add - Complete the resource configuration class

--- src/main/java/com/zfile/code/config/ErrorConfig.java
+++ src/main/java/com/zfile/code/config/ErrorConfig.java
@@ -6,9 +6,16 @@ import org.springframework.boot.web.server.ErrorPageRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+/**
+ * [异常配置类](Exception configuration class)
+ * @description: zh - 异常配置类
+ * @description: en - Exception configuration class
+ * @version: V1.0
+ * @author XiaoXunYao
+ * @since 2021/7/10 9:59 上午
+*/
 @Component
 public class ErrorConfig implements ErrorPageRegistrar {
-
 
     @Override
     public void registerErrorPages(ErrorPageRegistry registry) {
